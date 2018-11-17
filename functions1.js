84494823b07b1a5258fa1a3e8965de0657867598
CodeNext <codenext@Admins-MBP-10.lan>
2018-11-17 15:56:06 -0500

    11/18/17

--- functions1.js
+++ functions1.js
@@ -2,16 +2,33 @@ const READLINE = require("readline-sync");
 
 // **** Problem 1: printMessage() ****
 // Write a function that prints any message you want.
-
+let message = function(){
+	console.log("My name is Deziah")
+}
 
 // **** Problem 2: printFiveMessages() ****
 // Write a function that calls printMessage() five times.
-
+let printMessage = function(){
+message();
+message();
+message();
+message();
+message();
+}
+printMessage();
 
 // **** Problem 3: getUserInput() ****
 // Write a function that asks the user if they'd like to print your message
 // once or five times. Then call one of the two functions above based on
 // what the user decides.
+let user = READLINE.question("Would you like to print your message once or five times?")
+ if(user == "once" || user == "1"){
+ 	console.log(message())
+ }else{
+ 	console.log(printmessage());
+ }
+
+
 
 
 // **** Problem 4: printGreeting() ****
@@ -31,3 +48,5 @@ const READLINE = require("readline-sync");
 
 
 // When finished, call your run() function below to run the whole program!
+
+
